77d4d4107c1fd360f405b89bf732f041e3c8ce9b
Mehmet Serhat Arslan <63358327+Mserhatarslan@users.noreply.github.com>
2022-04-28 19:28:45 +0300

    Update merhaba.cpp

--- reptobot_ws/src/ilk_paket/src/merhaba.cpp
+++ reptobot_ws/src/ilk_paket/src/merhaba.cpp
@@ -1,31 +1,18 @@
 
-
 #include "ros/ros.h"
-
 int main( int argc, char **argv) /* C++'ın ana fonksiyonu çağırdık */
 /* arguman c ve arguman c */
-
 {
-
-
-    
-    
         ros::init(argc, argv, "Merhaba_ROS"); /* dosyayı başlattık */
         ros::NodeHandle nh; /* düğümün açılması kapatılması için kullandık */
         ros::Rate loop_rate(10); /* hız */
-
         int sayi=0;
         while (ros::ok) /* roscore çalıştığı sürece okey döndürür */
         {
-            ROS_INFO_STREAM(" Merhaba Mekatronikciler"<<sayi);  /* bilgilendirici bir mesaj döndürür */
-
+            ROS_INFO_STREAM(" Merhaba Dünya"<<sayi);  /* bilgilendirici bir mesaj döndürür */
             ros::spinOnce();
             loop_rate.sleep();
             sayi++;
-
         }
        return 0;
-        
-        
-    
 }
