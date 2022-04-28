fa54cf45c8df09931ec888c8c26a1c41f99c25d3
Mehmet Serhat Arslan <63358327+Mserhatarslan@users.noreply.github.com>
2022-04-28 19:29:33 +0300

    Update dinleyen1.cpp

--- reptobot_ws/src/dedikodu/src/dinleyen1.cpp
+++ reptobot_ws/src/dedikodu/src/dinleyen1.cpp
@@ -1,25 +1,18 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
-
 void konusmaCallback( const std_msgs::String::ConstPtr& msg);
-
 int main( int argc, char **argv)
 {
-
     ros::init(argc,argv,"Cok_Dinleyen");
     ros::NodeHandle nh;
     ros::Subscriber sub_dinleyen;
     sub_dinleyen = nh.subscribe("konusan_teyze", 1000,konusmaCallback);
     ros::spin();
-    return 0;
-    
+    return 0;    
 }
-
 
 void konusmaCallback( const std_msgs::String::ConstPtr& msg)
 {
-
     ROS_INFO( "Teyze ne dediğini duydum %s", msg->data.c_str());
-
 }
